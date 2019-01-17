ae9a73a4725b5e6421bc63c699cd057c857456d6
NavaneethRaj <46793363+NavaneethRaj@users.noreply.github.com>
2019-01-18 00:42:49 +0530

    Created new git commit for selenium code

---
+++ src/test/java/com/mymaven/app/APITest.java
@@ -0,0 +1,15 @@
+package com.mymaven.app;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void apiAuto() {
+		System.out.println("API Automation");
+	}
+	
+	@Test
+	public void apiUI() {
+		System.out.println("API elements");
+	}
+}
---
+++ src/test/java/com/mymaven/app/SeleniumTest.java
@@ -0,0 +1,16 @@
+package com.mymaven.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void seleniumAuto() {
+		System.out.println("Selenium Automation");
+	}
+	
+	@Test
+	public void seleniumUI() {
+		System.out.println("Selenium elements");
+	}
+}
